1cf5f8a1cb9d72c356da473d95cdee1e11fb42b9
Marshall Sykes <rmsykes91@gmail.com>
2019-10-02 11:23:37 -0400

    Assigned coins array from each player's object to the coins displayed on screen and removed text from html that it replaces. Also began working on if statements for clicks that will push coins to other arrays

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ document.querySelector('button').addEventListener('click', (e) => {
     let diceOneValues = ['L', 'R', 'C', '*', '*', '*']
     let randomDiceOneValue = diceOneValues[Math.floor(Math.random() * diceOneValues.length)]
     document.querySelector('#diceOne').innerHTML = randomDiceOneValue
+    if (randomDiceOneValue === '*') {
+        console.log('keep coin one!')
+    }
+    if (randomDiceOneValue === 'L') {
+        console.log('pass coin one left!')
+        
+    }
+    if (randomDiceOneValue === 'R') {
+        console.log('pass coin one right!')
+
+    }
+    if (randomDiceOneValue === 'C') {
+        console.log('pass coin one center!')
+
+    }
+
 
     let diceTwoValues = ['L', 'R', 'C', '*', '*', '*']
     let randomDiceTwoValue = diceTwoValues[Math.floor(Math.random() * diceTwoValues.length)]
@@ -18,20 +34,30 @@ document.querySelector('button').addEventListener('click', (e) => {
 
 let playerOne = {
     name: 'Player 1',
-    coins: ['x','x','x']
+    coins: [' X', ' X', ' X']
 }
 
 
 let playerTwo = {
     name: 'Player 2',
-    coins: ['x','x','x']
+    coins: [' X', ' X', ' X']
 }
 
 
 let playerThree = {
     name: 'Player 3',
-    coins: ['x','x','x']
+    coins: ['X', 'X', 'X']
 }
 
+
+// assings my array of coins to playerOneCoins div 
+document.querySelector('#playerOneCoins').innerText = playerOne.coins
+
+// assigns my array of coins to playerTwoCoins div
+document.querySelector('#playerTwoCoins').innerText = playerOne.coins
+
+
+// assigns my array of coins to playerThreeCoins div
+document.querySelector('#playerThreeCoins').innerText = playerOne.coins
 
 
