9525b46f12adb7bca3e164c6bf4f2b9de47e01eb
Marshall Sykes <rmsykes91@gmail.com>
2019-10-01 23:59:18 -0400

    Programmed the dice so that when the roll dice button is pressed each of the three dice is assigned a random value of L,R,C or *(x3)

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let dice = document.querySelector('#dice') 
 
+// Below code is an event listener and function that rolls each of the 3 dice and returns a ramdom value of L, R, C, or *(x3) and 
+// replaces the first in the display to the randomized dice value.
 document.querySelector('button').addEventListener('click', (e) => {
-    dice.classList.toggle('red')
+    let diceOneValues = ['L', 'R', 'C', '*', '*', '*']
+    let randomDiceOneValue = diceOneValues[Math.floor(Math.random() * diceOneValues.length)]
+    document.querySelector('#diceOne').innerHTML = randomDiceOneValue
+
+    let diceTwoValues = ['L', 'R', 'C', '*', '*', '*']
+    let randomDiceTwoValue = diceTwoValues[Math.floor(Math.random() * diceTwoValues.length)]
+    document.querySelector('#diceTwo').innerHTML = randomDiceTwoValue
+
+    let diceThreeValues = ['L', 'R', 'C', '*', '*', '*']
+    let randomDiceThreeValue = diceThreeValues[Math.floor(Math.random() * diceThreeValues.length)]
+    document.querySelector('#diceThree').innerHTML = randomDiceThreeValue
+
 })
+
