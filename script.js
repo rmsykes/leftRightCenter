7311ccf4e1a0eb6e337d5b6d7911b60c5cfef7b5
Marshall Sykes <rmsykes91@gmail.com>
2019-10-02 12:03:01 -0400

    Dice one now can move coin from Player1 array to player2 array with a L, player3 array with a R, and coinCenter with a C

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // Below code is an event listener and function that rolls each of the 3 dice and returns a ramdom value of L, R, C, or *(x3) and 
 // replaces the first in the display to the randomized dice value.
 document.querySelector('button').addEventListener('click', (e) => {
+    
+    // Dice One
     let diceOneValues = ['L', 'R', 'C', '*', '*', '*']
     let randomDiceOneValue = diceOneValues[Math.floor(Math.random() * diceOneValues.length)]
     document.querySelector('#diceOne').innerHTML = randomDiceOneValue
@@ -19,23 +21,38 @@ document.querySelector('button').addEventListener('click', (e) => {
     }
     if (randomDiceOneValue === 'R') {
         console.log('pass coin one right!')
-
+        let playerOneArray = playerOne.coins
+        playerOneArray.pop()
+        let playerThreeArray = playerThree.coins
+        playerThreeArray.push(' X')
+        document.querySelector('#playerOneCoins').innerText = playerOne.coins
+        document.querySelector('#playerThreeCoins').innerText = playerThree.coins
     }
     if (randomDiceOneValue === 'C') {
         console.log('pass coin one center!')
-
+        let playerOneArray = playerOne.coins
+        playerOneArray.pop()
+        let coinCenterArray = coinCenter.coins
+        coinCenterArray.push(' X')
+        document.querySelector('#playerOneCoins').innerText = playerOne.coins
+        document.querySelector('#coinCenter').innerText = coinCenter.coins
     }
 
-
+    // Dice Two
     let diceTwoValues = ['L', 'R', 'C', '*', '*', '*']
     let randomDiceTwoValue = diceTwoValues[Math.floor(Math.random() * diceTwoValues.length)]
     document.querySelector('#diceTwo').innerHTML = randomDiceTwoValue
 
+
+    // Dice Three
     let diceThreeValues = ['L', 'R', 'C', '*', '*', '*']
     let randomDiceThreeValue = diceThreeValues[Math.floor(Math.random() * diceThreeValues.length)]
     document.querySelector('#diceThree').innerHTML = randomDiceThreeValue
 })
 
+
+
+// Objects Below ////////////////////////////////////////////////////////////
 
 let playerOne = {
     name: 'Player 1',
@@ -51,8 +68,16 @@ let playerTwo = {
 
 let playerThree = {
     name: 'Player 3',
-    coins: ['X', 'X', 'X']
+    coins: [' X', ' X', ' X']
 }
+
+
+let coinCenter = {
+    name: 'Coin Center',
+    coins: []
+}
+//////////////////////////////////////////////////////////////////////////////
+
 
 
 // assings my array of coins to playerOneCoins div 
