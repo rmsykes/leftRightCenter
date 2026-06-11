70f917a0bb78e5388d2426ad8afd6896fba82b43
Marshall Sykes <54545905+rmsykes@users.noreply.github.com>
2026-06-11 16:59:05 -0400

    Refactor dice rolling and coin animation logic

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ const dice = {
 const diceSelectors = ['#diceOne', '#diceTwo', '#diceThree']
 const diceArea = document.querySelector('.dice')
 let activePlayerIndex = 0
+let isTurnInProgress = false
 
 function renderCoins() {
     players.forEach((player) => {
@@ -54,7 +55,17 @@ function renderCoins() {
 
 function renderDice(rolls = [dice.diceEmoji, dice.diceEmoji, dice.diceEmoji]) {
     diceSelectors.forEach((selector, index) => {
-        document.querySelector(selector).innerText = rolls[index] || ''
+        const die = document.querySelector(selector)
+        const roll = rolls[index] || ''
+
+        die.innerText = roll
+        die.classList.toggle('diceFace', Boolean(roll))
+        die.classList.remove('diceRoll')
+
+        if (roll) {
+            die.offsetHeight
+            die.classList.add('diceRoll')
+        }
     })
 }
 
@@ -84,7 +95,7 @@ function updateButtons() {
 
     players.forEach((player, index) => {
         const button = document.querySelector(player.buttonSelector)
-        button.disabled = Boolean(winner) || index !== activePlayerIndex || player.coins.length === 0
+        button.disabled = isTurnInProgress || Boolean(winner) || index !== activePlayerIndex || player.coins.length === 0
     })
 
     if (!winner && players[activePlayerIndex].coins.length === 0) {
@@ -93,37 +104,103 @@ function updateButtons() {
     }
 }
 
+function getElementCenter(selector) {
+    const element = document.querySelector(selector)
+    const rect = element.getBoundingClientRect()
+
+    return {
+        x: rect.left + rect.width / 2,
+        y: rect.top + rect.height / 2
+    }
+}
+
+function animateCoinMove(fromSelector, toSelector) {
+    const from = getElementCenter(fromSelector)
+    const to = getElementCenter(toSelector)
+    const movingCoin = document.createElement('div')
+
+    movingCoin.className = 'movingCoin'
+    movingCoin.innerText = coinDisplay
+    movingCoin.style.left = `${from.x}px`
+    movingCoin.style.top = `${from.y}px`
+    document.body.appendChild(movingCoin)
+
+    const animation = movingCoin.animate([
+        { transform: 'translate(-50%, -50%) scale(1)', left: `${from.x}px`, top: `${from.y}px` },
+        { transform: 'translate(-50%, -50%) scale(1.25)', offset: 0.5 },
+        { transform: 'translate(-50%, -50%) scale(1)', left: `${to.x}px`, top: `${to.y}px` }
+    ], {
+        duration: 650,
+        easing: 'ease-in-out',
+        fill: 'forwards'
+    })
+
+    return animation.finished.then(() => {
+        movingCoin.remove()
+    })
+}
+
 function applyRoll(player, roll) {
     if (roll === '*') {
-        return
+        return null
     }
 
     player.coins.pop()
 
     if (roll === 'L') {
-        players[player.leftPlayerIndex].coins.push(coinDisplay)
+        const targetPlayer = players[player.leftPlayerIndex]
+        targetPlayer.coins.push(coinDisplay)
+
+        return {
+            fromSelector: player.coinsSelector,
+            toSelector: targetPlayer.coinsSelector
+        }
     }
 
     if (roll === 'R') {
-        players[player.rightPlayerIndex].coins.push(coinDisplay)
+        const targetPlayer = players[player.rightPlayerIndex]
+        targetPlayer.coins.push(coinDisplay)
+
+        return {
+            fromSelector: player.coinsSelector,
+            toSelector: targetPlayer.coinsSelector
+        }
     }
 
     if (roll === 'C') {
         coinCenter.coins.push(coinDisplay)
+
+        return {
+            fromSelector: player.coinsSelector,
+            toSelector: '#coinCenter'
+        }
     }
 }
 
 function rollDiceForPlayer(player) {
     const diceCount = Math.min(player.coins.length, 3)
     const rolls = []
+    const coinMoves = []
 
     for (let index = 0; index < diceCount; index++) {
         const roll = dice.diceValues[Math.floor(Math.random() * dice.diceValues.length)]
         rolls.push(roll)
-        applyRoll(player, roll)
+        const coinMove = applyRoll(player, roll)
+
+        if (coinMove) {
+            coinMoves.push(coinMove)
+        }
     }
 
     renderDice(rolls)
+
+    return coinMoves
+}
+
+async function animateCoinMoves(coinMoves) {
+    for (const coinMove of coinMoves) {
+        await animateCoinMove(coinMove.fromSelector, coinMove.toSelector)
+    }
 }
 
 function finishTurn() {
@@ -142,7 +219,7 @@ function finishTurn() {
     updateButtons()
 }
 
-function takeTurn(playerIndex) {
+async function takeTurn(playerIndex) {
     if (playerIndex !== activePlayerIndex) {
         return
     }
@@ -155,8 +232,12 @@ function takeTurn(playerIndex) {
         return
     }
 
+    isTurnInProgress = true
+    updateButtons()
     setDiceTurnColor(player)
-    rollDiceForPlayer(player)
+    const coinMoves = rollDiceForPlayer(player)
+    await animateCoinMoves(coinMoves)
+    isTurnInProgress = false
     finishTurn()
 }
 
